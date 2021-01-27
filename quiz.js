eaceca504b1f604c214b785f70368cf330465b1b
Shahzadrori <shahzadrori@gmail.com>
2021-01-27 15:53:59 +0500

    update

--- quiz.js
+++ quiz.js
@@ -1,34 +1,76 @@
 const QuizData = [
   {
     id: 0,
-    question: ` What is the Capital Of India ?`,
-    options: [`New Delhi`, `Mumbai`, `Kolkatta`],
-    answer: `New Delhi`,
+    question: ` What is the Capital Of Pakistan ?`,
+    options: [`Islamabad`, `Multan`, `Karachi`, "Lahore"],
+    ans: `ans1`,
   },
   {
     id: 1,
     question: `Who is the CEO of Tesla Motors?`,
     options: [`Bill Gates`, `Steve Jobs`, `Elon Musk`],
-    answer: `Elon Musk`,
+    ans: `ans4`,
   },
   {
     id: 3,
     question: `Name World's Richest Man?`,
     options: [`Jeff Bezo`, `Bill Gates`, `Mark Zuckerberg`],
-    answer: `Jeff Bezo`,
+    ans: `ans1`,
   },
   {
     id: 4,
     question: `World's Longest River?`,
-    options: [`River Nile`, `River Amazon`, `River Godavari`],
-    answer: `River Nile`,
+    options: [`River Amazon`, `River Nile`, `River Godavari`],
+    ans: `ans2`,
   },
 ];
+console.log(QuizData.l)
 
-var quwetion = document.querySelector(".ques");
-var option1 = document.querySelector("#ans1");
-var option2 = document.querySelector("#ans2");
-var option3 = document.querySelector("#ans3");
-var option4 = document.querySelector("#ans4");
-var option5 = document.querySelector("#ans5");
-var submit= document.querySelector('#sub2');
+var question = document.querySelector(".ques");
+var option1 = document.querySelector(".option1");
+var option2 = document.querySelector(".option2");
+var option3 = document.querySelector(".option3");
+var option4 = document.querySelector(".option4");
+var submit = document.querySelector("#sub");
+var answers = document.querySelectorAll(".answers");
+var showscore = document.querySelector('#showscore')
+
+let questioncount= 0;
+let score  = 0;
+const loaddata = () => {
+  question.innerHTML = QuizData[questioncount].question;
+  option1.innerHTML = QuizData[questioncount].options[0];
+  option2.innerHTML = QuizData[questioncount].options[1];
+  option3.innerHTML = QuizData[questioncount].options[2];
+  // option4.innerHTML = QuizData[questioncount].options[3];
+};
+
+loaddata();
+const getcheckans = () => {
+  let jawab;
+  answers.forEach((currentelement) => {
+    if (currentelement.checked) {
+      jawab = currentelement.id
+    }
+  });
+  return jawab;
+};
+
+submit.addEventListener("click", () => {
+  const getcheck = getcheckans();
+  console.log(getcheck);
+  if(getcheck === QuizData[questioncount].ans){
+      score++
+  }
+  questioncount++;
+  if(questioncount < QuizData.length  ){
+      loaddata();
+  }
+  else{
+      showscore.innerHTML = `
+      <h2 class='res'>Your scored ${score}/${ QuizData.length} 😮
+      <button class='tub' onclick='location.reload()'>Try Again</button>
+      `;
+      showscore.classList.remove('showarea')
+  }
+});

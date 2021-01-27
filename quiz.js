1700c606ef6846b56bef947bbec9b54cf1b463fc
Shahzadrori <shahzadrori@gmail.com>
2021-01-27 11:58:09 +0500

    update

--- quiz.js
+++ quiz.js
@@ -1,27 +1,34 @@
- const QuizData = [
-    {
-      id: 0,
-      question: ` What is the Capital Of India ?`,
-      options: [`New Delhi`, `Mumbai`, `Kolkatta`],
-      answer: `New Delhi`
-    },
-    {
-      id: 1,
-      question: `Who is the CEO of Tesla Motors?`,
-      options: [`Bill Gates`, `Steve Jobs`, `Elon Musk`],
-      answer: `Elon Musk`
-    },
-    {
-      id: 3,
-      question: `Name World's Richest Man?`,
-      options: [`Jeff Bezo`, `Bill Gates`, `Mark Zuckerberg`],
-      answer: `Jeff Bezo`
-    },
-    {
-      id: 4,
-      question: `World's Longest River?`,
-      options: [`River Nile`, `River Amazon`, `River Godavari`],
-      answer: `River Nile`
-    }
-  ];
+const QuizData = [
+  {
+    id: 0,
+    question: ` What is the Capital Of India ?`,
+    options: [`New Delhi`, `Mumbai`, `Kolkatta`],
+    answer: `New Delhi`,
+  },
+  {
+    id: 1,
+    question: `Who is the CEO of Tesla Motors?`,
+    options: [`Bill Gates`, `Steve Jobs`, `Elon Musk`],
+    answer: `Elon Musk`,
+  },
+  {
+    id: 3,
+    question: `Name World's Richest Man?`,
+    options: [`Jeff Bezo`, `Bill Gates`, `Mark Zuckerberg`],
+    answer: `Jeff Bezo`,
+  },
+  {
+    id: 4,
+    question: `World's Longest River?`,
+    options: [`River Nile`, `River Amazon`, `River Godavari`],
+    answer: `River Nile`,
+  },
+];
 
+var quwetion = document.querySelector(".ques");
+var option1 = document.querySelector("#ans1");
+var option2 = document.querySelector("#ans2");
+var option3 = document.querySelector("#ans3");
+var option4 = document.querySelector("#ans4");
+var option5 = document.querySelector("#ans5");
+var submit= document.querySelector('#sub2');
